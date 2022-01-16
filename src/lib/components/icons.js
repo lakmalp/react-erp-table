6dac768883c13b0ba23cf1618d92a4c067a7264e
lakmalp <lakmalp@gmail.com>
2022-01-16 07:43:05 +0530

    added missing references

--- src/lib/components/icons.js
+++ src/lib/components/icons.js
@@ -1,3 +1,5 @@
+import React from "react"
+
 export const IconDuplicate = ({ className, color, width }) => {
   return (
     <svg className={className} width={width} fill={color} id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100.32 122.88">
